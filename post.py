6847e6e447a25a895299a4a2472c742298e51f1f
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2019-01-20 01:47:36 -0600

    Update post.py

--- post.py
+++ post.py
@@ -3,15 +3,20 @@
 import sys
 import tweepy
 
+# Consumer keys and access tokens, used for OAuth
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
 ACCESS_TOKEN_KEY = ''
 ACCESS_TOKEN_SECRET = ''
 
+# OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+
+# Creation of the actual interface, using authentication
 api = tweepy.API(auth)
-argentinaFlag = u'\U0001F1E6' + u'\U0001F1F7'
+
+#argentinaFlag = u'\U0001F1E6' + u'\U0001F1F7'
 
 filenames = []
 for element in sys.argv[3:]:
@@ -22,4 +27,5 @@ for filename in filenames:
   res = api.media_upload(filename)
   media_ids.append(res.media_id)
 
+  # Sample method, used to update a status
 api.update_status(status=argentinaFlag + ' Imagen satelital: ' + sys.argv[1] + '. Elevacion maxima: ' + sys.argv[2] + ' grados. #NOAA #weather #argentinaimagenes #noaasatellite #clima #wxtoimg #raspberrypi #argentina #argentinasat', media_ids=media_ids)
