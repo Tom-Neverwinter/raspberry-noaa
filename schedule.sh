0378002263ae2ecbfe8d2ab401ab0acb2556378f
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2019-01-20 00:34:57 -0600

    fix frequencies
    changed to actual frequencies based on google.

--- schedule.sh
+++ schedule.sh
@@ -15,7 +15,7 @@ grep "METEOR-M 2" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/weather/
 for i in $(atq | awk '{print $1}');do atrm "$i";done
 
 #Schedule Satellite Passes:
-"${NOAA_HOME}"/schedule_sat.sh "NOAA 19" 137.1000
+"${NOAA_HOME}"/schedule_sat.sh "NOAA 19" 137.3500
 "${NOAA_HOME}"/schedule_sat.sh "NOAA 18" 137.9125
 "${NOAA_HOME}"/schedule_sat.sh "NOAA 15" 137.6200
-"${NOAA_HOME}"/schedule_sat.sh "METEOR-M 2" 137.1
+"${NOAA_HOME}"/schedule_sat.sh "METEOR-M 2" 137.100
