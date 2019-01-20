341bdccf5e6b2b9c47f63c131055ea760f3df886
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2019-01-20 00:32:30 -0600

    re add meteor m2
    why was this removed.... comment it out!

--- schedule.sh
+++ schedule.sh
@@ -9,6 +9,7 @@ wget -qr https://www.celestrak.com/NORAD/elements/weather.txt -O "${NOAA_HOME}"/
 grep "NOAA 15" "${NOAA_HOME}"/predict/weather.txt -A 2 > "${NOAA_HOME}"/predict/weather.tle
 grep "NOAA 18" "${NOAA_HOME}"/predict/weather.txt -A 2 >> "${NOAA_HOME}"/predict/weather.tle
 grep "NOAA 19" "${NOAA_HOME}"/predict/weather.txt -A 2 >> "${NOAA_HOME}"/predict/weather.tle
+grep "METEOR-M 2" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/weather/predict/weather.tle
 
 #Remove all AT jobs
 for i in $(atq | awk '{print $1}');do atrm "$i";done
@@ -17,3 +18,4 @@ for i in $(atq | awk '{print $1}');do atrm "$i";done
 "${NOAA_HOME}"/schedule_sat.sh "NOAA 19" 137.1000
 "${NOAA_HOME}"/schedule_sat.sh "NOAA 18" 137.9125
 "${NOAA_HOME}"/schedule_sat.sh "NOAA 15" 137.6200
+"${NOAA_HOME}"/schedule_sat.sh "METEOR-M 2" 137.1
